462e763b13aca1de13dadefc0be62f6535ba4905
jameshdugulu <jameshdugulu@gmail.com>
2025-11-18 16:35:06 +0200

    changing color at internship

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (  
     <> 
     <div className="app">
-      <strong>Shop</strong>
+      <strong>Internship Project</strong>
       <Headers />
       <main className="main">
         <Routes>
